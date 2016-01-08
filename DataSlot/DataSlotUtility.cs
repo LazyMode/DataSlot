ef0534e1247c2474fb8ec4eb353f64fea8056305
Licshee <licshee@yen.ai>
2016-01-08 16:47:18 +0800

    class DataSlotUtility

---
+++ DataSlot/DataSlotUtility.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
+
+public static class DataSlotUtility
+{
+    private static readonly ConditionalWeakTable<object, ConcurrentDictionary<object, object>> MapOfDataSlotTables
+        = new ConditionalWeakTable<object, ConcurrentDictionary<object, object>>();
+
+    public static object GetSlotData<T>(this T target, object slot)
+        where T : class
+    {
+        ConcurrentDictionary<object, object> table;
+        if (!MapOfDataSlotTables.TryGetValue(target, out table))
+            return null;
+        return table[slot];
+    }
+    public static void SetSlotData<T>(this T target, object slot, object data)
+        where T : class
+        => MapOfDataSlotTables.GetOrCreateValue(target)[slot] = data;
+    public static bool TryGetSlotData<T>(this T target, object slot, out object data)
+        where T : class
+    {
+        ConcurrentDictionary<object, object> table;
+        if (MapOfDataSlotTables.TryGetValue(target, out table))
+            return table.TryGetValue(slot, out data);
+
+        data = null;
+        return false;
+    }
+}
+
